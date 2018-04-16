d947eb4faefac95c8b14ff4c11bfb4e02000cc49
Jacob <jacob@jacobolofsson.se>
2018-04-16 19:02:50 +0200

    Added AI functions and comments

--- othello_bot/AI.cpp
+++ othello_bot/AI.cpp
@@ -1,26 +1,58 @@
 #include "AI.hpp"
+
 #include "random.hpp"
 
-AI::AI(Game g, Player p) : game(g), AIplayer(p) {
+// The AI is implemented by using a Monte-Carlo strategy. Random moves
+// are applied until the game ends and the AI records if it was a win
+// or lose. The best move is calculated by simply assigning each legal
+// move with a score, which is the number of simulations starting with
+// this move divided with the number that resulted in a win.
+
+// WHen an AI object is constructed it adds all the legal moves from the
+// current game supplied to the simulations vector
+AI::AI(Game g, Player p) : currentGame(g), AIplayer(p) {
     Coordinate tempBuffer[BUFFER_SIZE];
-    nAvaliableMoves = game.getLegalMoves(AIplayer, tempBuffer);
+    nAvaliableMoves = currentGame.getLegalMoves(AIplayer, tempBuffer);
     for (int i = 0; i<nAvaliableMoves; i++) {
-        //simulations[i](g, p, tempBuffer[i]);
         simulations[i].startCoord = tempBuffer[i];
         reset(simulations[i]);
     };
 };
+// This function steps the AIs current simulation one step.
+// If the simulation is over the AI adds the score to the simulation
+// vector, resets the game and then switches the current simulation
+// to the next in turn (circularly).
 void AI::think(void) {
-    if( simulations[currentSim].game.isOver() ) {
-        if( simulations[currentSim].game.isWinner(AIplayer) ) {
-            simulations[currentSim].nWins++;
-        }
-        simulations[currentSim].nSimulations++;
-        reset(simulations[currentSim]);
-        currentSim = (currentSim + 1)%nAvaliableMoves;
+    if( nAvaliableMoves > 0) {
+        if( simulations[currentSim].game.isOver() ) {
+            if( simulations[currentSim].game.isWinner(AIplayer) ) {
+                simulations[currentSim].nWins++;
+            }
+            simulations[currentSim].nSimulations++;
+            reset(simulations[currentSim]);
+            currentSim = (currentSim + 1)%nAvaliableMoves;
+        };
+        step(simulations[currentSim]);
     };
-    step(simulations[currentSim]);
 };
+// This function iterates through the simulation vector that contains the
+// the coordinates for all the legal moves and the scores associated with
+// them. It picks the one with best score.
+Coordinate AI::getBestMove(void) const {
+    float bestScore = -1000.0;
+    int bestIndex = -1;
+    float score = 0.0;
+    for (int i = 0; i<nAvaliableMoves; i++) {
+        score = simulations[i].getScore();
+        if ( score > bestScore) {
+            bestScore = score;
+            bestIndex = i;
+        };
+    };
+    return simulations[bestIndex].startCoord;
+};
+// This steps the simulation forward one step by applying one legal move and
+// then switching player.
 void AI::step(Simulation s) {
     Coordinate tempBuffer[BUFFER_SIZE];
     int tempAvaliableMoves = s.game.getLegalMoves(s.turn, tempBuffer);
@@ -29,10 +61,10 @@ void AI::step(Simulation s) {
     }
     s.turn = Game::nextPlayer(s.turn);
 };
+// This function resets a simulation by setting the game to the input game
+// of the AI and applying the starting move of the simulation.
 void AI::reset(Simulation s) {
-    s.game = this->game;
+    s.game = this->currentGame;
     s.game.applyMove(this->AIplayer, s.startCoord);
     s.turn = Game::nextPlayer(this->AIplayer);
 };
-
-//AI::Simulation::Simulation(const Game g, const Player p, const Coordinate c) : game(g), turn(p), startCoord(c) {};
--- othello_bot/AI.hpp
+++ othello_bot/AI.hpp
@@ -3,30 +3,45 @@
 
 #include "game.hpp"
 
+// An AI that will provide the best move for a game of othello
+// given enough time to think.
+// Example:
+//      AI ai = AI(game, player);
+//      for (int i = 0; i<1000; ++i) {
+//          ai.think();
+//      };
+//      bestMove = getBestMove();
 class AI {
     public:
         AI(Game g, Player p);
 
         void think(void);
-
+        int getNumMoves(void) const { return nAvaliableMoves; };
         Coordinate getBestMove(void) const;
+
     private:
-        const Game game;
-        const Player AIplayer;
+        // Structure that holds data for the simulations the AI
+        // uses to calculate the best move
         struct Simulation {
-            //Simulation(Game g, Player p, Coordinate c);
             Game game {Game(Board<4,4>())};
             Player turn;
             Coordinate startCoord;
             int nSimulations {0};
             int nWins {0};
+            float getScore(void) const {
+                return nSimulations == 0 ? 0 : nWins / (float) nSimulations;
+            };
         };
-        void step(Simulation s);
-        void reset(Simulation s);
+
+        const Game currentGame;
+        const Player AIplayer;
         static const int BUFFER_SIZE = 20;
         Simulation simulations[BUFFER_SIZE];
-        int currentSim;
-        int nAvaliableMoves;
+        int currentSim {0};
+        int nAvaliableMoves {0};
+
+        void step(Simulation s);
+        void reset(Simulation s);
 };
 
 #endif // AI_H
