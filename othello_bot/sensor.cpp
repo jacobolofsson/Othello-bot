cfbc0126332c6f75e17129c1421c52efe6b0d115
Jacob <jacob@jacobolofsson.se>
2018-04-20 14:57:50 +0200

    Implemented sensor funcitons

--- othello_bot/sensor.cpp
+++ othello_bot/sensor.cpp
@@ -1,13 +1,33 @@
 #include "sensor.hpp"
+#include "game.hpp"
 
 #include "Arduino.h"
 
-Sensor::Sensor(SensorArray arr) : sensors(arr) {};
+constexpr int Sensor::pins[];
 
 void Sensor::setup(void) {
-    for (int i = 0; i <16; ++i) {
-        pinMode(sensors.pin[i], INPUT_PULLUP);
+    for (int i = 0; i <N_SENSORS; ++i) {
+        pinMode(pins[i], INPUT);
     };
 };
-void Sensor::read(void) {};
-Board<4,4> Sensor::getBoard(void) const {};
+void Sensor::read(void) {
+    sensorVals[sensorId] = analogRead(pins[sensorId]);
+    sensorId = (sensorId + 1)%N_SENSORS;
+};
+Board<4,4> Sensor::getBoard(void) const {
+    Board<4,4> board;
+    for (int i = 0; i < N_SENSORS; ++i) {
+        if (sensorVals[i] > UPPER_LIM) {
+            board.put(BLACK, pinToCoord(i));
+        } else if (sensorVals[i] < LOWER_LIM) {
+            board.put(WHITE, pinToCoord(i));
+        };
+    };
+    return board;
+};
+Coordinate Sensor::pinToCoord(const int pin) {
+    Coordinate c;
+    c.rowPos = pin % 4;
+    c.colPos = (pin - (pin%4))/4;
+    return c;
+};
--- othello_bot/sensor.hpp
+++ othello_bot/sensor.hpp
@@ -1,21 +1,23 @@
 #ifndef SENSOR_H
 #define SENSOR_H
 
+#include "config.hpp"
 #include "game.hpp"
-        struct SensorArray {
-            const int pin[16];
-        };
-
 class Sensor {
     public:
-        Sensor(SensorArray arr);
-
         void setup(void);
         void read(void);
         Board<4,4> getBoard(void) const;
 
     private:
-        Board<4,4> board;
-        const SensorArray sensors;
+        static constexpr int UPPER_LIM = CONF_UPPER_SENS_LIM;
+        static constexpr int LOWER_LIM = CONF_LOWER_SENS_LIM;
+        static constexpr int N_SENSORS = CONF_N_SENSORS;
+        static constexpr int pins[N_SENSORS] = CONF_SENSOR_PINS_V;
+
+        int sensorVals[N_SENSORS];
+        int sensorId = 0;
+
+        static Coordinate pinToCoord(const int pin);
 };
 #endif // SENSOR_H
