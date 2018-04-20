ee54d5f1021339eccccb814f81d90a08cb2fc0e1
Jacob <jacob@jacobolofsson.se>
2018-04-21 01:15:53 +0200

    Added some stuff for the move planner

--- othello_bot/move_planner.cpp
+++ othello_bot/move_planner.cpp
@@ -1,6 +1,63 @@
 #include "move_planner.hpp"
 
-MovePlanner::MovePlanner(Stepper x, Stepper y, Servo z) : xMotor(x), yMotor(y), zMotor(z) {};
-void MovePlanner::setup(void) {};
-void MovePlanner::write(void) {};
-void MovePlanner::addMove(Coordinate c, Player p) {};
+#include "config.hpp"
+
+MovePlanner::MovePlanner() {};
+void MovePlanner::setup(void) {
+    xMotor.setSpeed(CONF_STEPPER_RPM);
+    yMotor.setSpeed(CONF_STEPPER_RPM);
+    zMotor.attach(CONF_Z_SERVO_PIN);
+    reset();
+};
+void MovePlanner::write(void) {
+    int xSteps;
+    int ySteps;
+    if (!isDone()) {
+        switch(actQueue[currentActionId]) {
+            case MOVE_TO_TARGET:
+                // To get better control keep steps per call to a maximum
+                xSteps = constrain(targetX-currentX, -CONF_MAX_STEPS, CONF_MAX_STEPS);
+                ySteps = constrain(targetY-currentY, -CONF_MAX_STEPS, CONF_MAX_STEPS);
+                xMotor.step(xSteps);
+                yMotor.step(ySteps);
+                currentX += xSteps;
+                currentY += ySteps;
+                if ( (xSteps == 0) && (ySteps == 0) ) {
+                    currentActionId++;
+                };
+                break;
+            case LOWER_ARM:
+                zMotor.write(CONF_SERVO_MAX);
+                //TODO Do we ned a delay here?
+                currentActionId++;
+                break;
+            case RAISE_ARM:
+                zMotor.write(0);
+                currentActionId++;
+                break;
+            case PICK_PIECE:
+                currentPolarity = targetPolarity;
+                break;
+            case DROP_PIECE:
+                currentPolarity = (targetPolarity == NEGATIVE) ? POSITIVE : NEGATIVE;
+                break;
+        };
+    };
+    //TODO something with the magnet??
+};        
+void MovePlanner::addMove(Coordinate c, Player p) {
+    targetX = c.rowPos*CONF_STEPS_PER_COORD_X;
+    targetY = c.colPos*CONF_STEPS_PER_COORD_Y;
+    targetPolarity = (p == WHITE) ? POSITIVE : NEGATIVE;
+    currentActionId = 0;
+    enum Action newActions[] = {MOVE_TO_TARGET, LOWER_ARM, DROP_PIECE, RAISE_ARM};
+    actQueueSize = sizeof(newActions)/sizeof(newActions[0]);
+    for (int i = 0; i < actQueueSize; ++i) {
+        actQueue[i] = newActions[i];
+    };
+
+};
+bool MovePlanner::isDone(void) {
+    return currentActionId >= actQueueSize;
+};
+void MovePlanner::reset(void) {};
--- othello_bot/move_planner.hpp
+++ othello_bot/move_planner.hpp
@@ -1,26 +1,37 @@
 #ifndef MOVE_PLANNER_H
 #define MOVE_PLANNER_H
 
+#include "config.hpp"
 #include "game.hpp"
 
-#define N_STEPPER_STEPS 100 
 #include <Stepper.h>
 #include <Servo.h>
 
 class MovePlanner {
     public:
-        MovePlanner(Stepper x, Stepper y, Servo z);
+        MovePlanner();
 
         void setup(void);
         void write(void);
         void addMove(Coordinate c, Player p); 
+        bool isDone(void);
 
     private:
-        const Stepper xMotor;
-        const Stepper yMotor;
-        const Servo zMotor;
+        Stepper xMotor = Stepper(CONF_STEPPER_STEPS, CONF_X_STEPPER_PINS);
+        Stepper yMotor = Stepper(CONF_STEPPER_STEPS, CONF_Y_STEPPER_PINS);
+        Servo zMotor = Servo();
 
-        enum action {X_STEP_FORWARD, X_STEP_BACKWARD, Y_STEP_FORWARD, Y_STEP_BACKWARD, PICK_PIECE, DROP_PIECE};
+        enum Action {MOVE_TO_TARGET, LOWER_ARM, RAISE_ARM, PICK_PIECE, DROP_PIECE};
+        enum Action actQueue[CONF_PLANNER_QUEUE_SZ];
+        int actQueueSize = 0;
+        int currentActionId;
+        enum Magnet {NEGATIVE, POSITIVE};
+        int targetX;
+        int targetY;
+        enum Magnet targetPolarity;
+        enum Magnet currentPolarity;
+        int currentX;
+        int currentY;
 
         void reset(void);
 };
